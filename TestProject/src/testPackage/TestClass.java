8eb324ff3019b1dd72f8582d13951c1f4f387de6
dikshaChandekar <DikshaChandekar54@gmail.com>
2023-03-04 23:29:20 +0530

    Basic task complete

---
+++ TestProject/src/testPackage/TestClass.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Testbase class");
+		System.out.println("util class");
+		System.out.println("TestLayer class");
+		System.out.println("PageLayer class");
+
+	}
+
+}
